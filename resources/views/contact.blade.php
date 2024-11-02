6a7b5628ceadabe445b65972aedea8746d67d388
revalian <revalian@gmail.com>
2024-11-03 00:50:51 +0700

    (VIDEO 4) Belajar Laravel 11 | 4. Blade Templating Engine

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+    
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Contact</h1>
+    <ul>
+        <li>
+          <a href="https://instagram.com/revalian_">Instagram</a>
+        </li>
+
+        <li>
+            <a href="https://x.com/dxlwyrs">X</a>
+        </li>
+
+        <li>
+            <a href="https://youtube.com/@lian2888">Youtube</a>
+        </li>
+    </ul>
+</body>
+</html>
