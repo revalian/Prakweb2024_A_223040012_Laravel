2aba118b412a56efa10ee0485c35fad36f6321fc
revalian <revalian@gmail.com>
2024-11-03 14:45:29 +0700

    (VIDEO 8) Belajar Laravel 11 | 8. Database & Migration

--- database/migrations/2024_11_03_073020_create_posts_table.php
+++ database/migrations/2024_11_03_073020_create_posts_table.php
@@ -11,15 +11,14 @@ return new class extends Migration
      *
      * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('title');
+            $table->string('author');
+            $table->string('slug')->unique();
+            $table->text('body');
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 };
