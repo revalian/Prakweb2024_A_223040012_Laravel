c2552a8df4e6e5ba1f9503ab8cb7c58d357b400e
revalian <revalian@gmail.com>
2024-11-03 08:58:15 +0700

    (VIDEO 5) Belajar Laravel 11 | 5. Blade Component

--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('/about', function () {
-    return view('about', ['nama' => 'Revalian Ananda']);
+    return view('about', ['nama' => 'Revalian Ananda', 'title' => 'About']);
 });
 
 Route::get('/blog', function () {
-    return view('blog');
+    return view('blog', ['title' => 'Blog']);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
